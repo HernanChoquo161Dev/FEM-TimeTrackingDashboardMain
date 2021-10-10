a76e263f1292defd458a67968c4940be3d4ba8ae
Hernan <hernanchoquedev@gmail.com>
2021-10-09 22:24:34 -0300

    add solution responsive design - CSS

--- js/script.js
+++ js/script.js
@@ -1,29 +1,73 @@
-
-
-/*fetch("../json/data.json")
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(error => console.log(error));*/
-  
 fetch("../json/data.json")
   .then(response => response.json())
   .then(data => {
     const tittleTracking = document.querySelectorAll(".cell-theme__text");
-    const timeTracking = document.querySelectorAll(".time-tracking__time");
-    const trackingSummary = document.querySelectorAll(".time-tracking__text");
+    const timeTracking = document.querySelectorAll(".time-tracking__count-time");
+    const trackingSummary = document.querySelectorAll(".time-tracking__text-count");
 
     for(a = 0; a < tittleTracking.length; a++){
         tittleTracking[a].textContent = data[a].title;
-        timeTracking[a].textContent = data[a].timeframes.weekly.current + "hrs";
-        trackingSummary[a].textContent = "Last Week - " + data[a].timeframes.weekly.previous + "hrs";
+        timeTracking[a].textContent = data[a].timeframes.weekly.current;
+        trackingSummary[a].textContent = data[a].timeframes.weekly.previous;
     }
-
-    console.log(data);
-    console.log(data[0]);
-    console.log(data[0].timeframes.weekly);
   })
   .catch(error => console.log(error));
 
+  const changeTimeList = document.querySelectorAll(".time-change__change-time");
+  var optionActive = 1;
 
-  sendForm.addEventListener('click', function (event) {
-});
+  function animationChangeDashboard(){
+    const textHidden = document.querySelectorAll(".time-tracking__time");
+
+    for(d = 0; d < textHidden.length; d++){
+        textHidden[d].classList.toggle("time-tracking__time--animation");
+    }
+  }
+
+  function changeDataDashboard(changeOption){
+    fetch("../json/data.json")
+    .then(response => response.json())
+    .then(data => {
+        const timeTracking = document.querySelectorAll(".time-tracking__count-time");
+        const trackingSummary = document.querySelectorAll(".time-tracking__text-count");
+
+        const textHidden2 = document.querySelectorAll(".time-tracking__time");
+  
+        for(c = 0; c < timeTracking.length; c++){
+            textHidden2[c].classList.toggle("time-tracking__time--animation");
+
+            if(changeOption == 0){
+                timeTracking[c].textContent = data[c].timeframes.daily.current;
+                trackingSummary[c].textContent = data[c].timeframes.daily.previous;
+            }
+            else if(changeOption == 1){
+                timeTracking[c].textContent = data[c].timeframes.weekly.current;
+                trackingSummary[c].textContent = data[c].timeframes.weekly.previous;
+            }
+            else{
+                timeTracking[c].textContent = data[c].timeframes.monthly.current;
+                trackingSummary[c].textContent = data[c].timeframes.monthly.previous;
+            }
+        }
+    })
+    .catch(error => console.log(error));
+  }
+
+  function activeOptionTime(index){
+    changeTimeList[index].addEventListener('click', function (event) {
+
+        animationChangeDashboard();
+
+        changeDataDashboard(index);
+
+        const optionTimeList = document.querySelectorAll(".time-change__option");
+        optionTimeList[optionActive].style.color = "hsl(236, 100%, 87%)";
+        optionTimeList[index].style.color = "#ffffff";
+
+        optionActive = index;
+    });
+  }
+
+  for(b = 0; b < changeTimeList.length; b++){
+    activeOptionTime(b);
+  }
